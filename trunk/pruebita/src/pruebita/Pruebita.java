e4f04ac4b8990f59fd26ebc872e3969bc581d351
jonatan.gonzales@pucp.pe <jonatan.gonzales@pucp.pe@f9317526-7256-5adc-a38a-648fc19730a4>
2012-10-03 13:40:45 +0000

    Este es mi proyecto

---
+++ trunk/pruebita/src/pruebita/Pruebita.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebita;
+
+/**
+ *
+ * @author Jonatan
+ */
+public class Pruebita {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Este probadita es mia");
+    }
+}
